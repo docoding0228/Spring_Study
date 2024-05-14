bd390ba412c20cda99be28fdb542961e41ab5483
docoding0228 <npng0228@naver.com>
2024-05-14 21:35:23 +0900

    get method 요청

--- src/main/java/com/sparta/springprepare/controller/HelloController.java
+++ src/main/java/com/sparta/springprepare/controller/HelloController.java
@@ -1,12 +1,28 @@
 package com.sparta.springprepare.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+// @RestController
+@Controller
 public class HelloController {
+//    @GetMapping("/api/hello")
+//    public String hello() {
+//        return "Hello World!!";
+//    }
+
+    @GetMapping("/api/get")
+    @ResponseBody
+    public String get() {
+        return "get method 요청";
+    }
+
     @GetMapping("/api/hello")
+    @ResponseBody
     public String hello() {
-        return "Hello World!!";
+        return "Hello World!!!!!";
     }
 }
+
+
