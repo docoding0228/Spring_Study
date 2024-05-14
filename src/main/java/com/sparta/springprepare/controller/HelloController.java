6a04eeae691d999f859244afde12eda721460a12
docoding0228 <npng0228@naver.com>
2024-05-14 21:51:30 +0900

    Get, Post, Put, Delete

--- src/main/java/com/sparta/springprepare/controller/HelloController.java
+++ src/main/java/com/sparta/springprepare/controller/HelloController.java
@@ -1,10 +1,10 @@
 package com.sparta.springprepare.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 // @RestController
+@RequestMapping("/api")
 @Controller
 public class HelloController {
 //    @GetMapping("/api/hello")
@@ -12,16 +12,35 @@ public class HelloController {
 //        return "Hello World!!";
 //    }
 
-    @GetMapping("/api/get")
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World!!!!!";
+    }
+
+    @GetMapping("/get")
     @ResponseBody
     public String get() {
         return "get method 요청";
     }
 
-    @GetMapping("/api/hello")
+    @PostMapping("/post")
     @ResponseBody
-    public String hello() {
-        return "Hello World!!!!!";
+    public String post() {
+        return "post method 요청";
+    }
+
+    @PutMapping("/put")
+    @ResponseBody
+    public String put() {
+        return "put method 요청";
+    }
+
+    @DeleteMapping("/api/delete")
+    @ResponseBody
+    // http://localhost:8080/api/api/delete 으로 출력해야 출력된다.
+    public String Delete() {
+        return "Delete method 요청";
     }
 }
 
