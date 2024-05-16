7993e30c2332de542288be4f48ea18862725870c
docoding0228 <npng0228@naver.com>
2024-05-16 16:15:43 +0900

    HTTP 데이터를 객체로 처리하는 방법

--- src/main/java/com/sparta/springprepare/request/RequestController.java
+++ src/main/java/com/sparta/springprepare/request/RequestController.java
@@ -1,11 +1,11 @@
 package com.sparta.springprepare.request;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/hello/request")
 public class RequestController {
-
     @GetMapping("/form/html")
     public String helloForm() {
         return "hello-request-form";
@@ -20,8 +20,8 @@ public class RequestController {
         return String.format("Hello, @PathVariable.<br> name = %s, age = %d", name, age);
     }
 
-//    // [Request sample]
-//    // GET http://localhost:8080/hello/request/form/param?name=Robbie&age=95
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param?name=Robbie&age=95
     @GetMapping("/form/param")
     @ResponseBody
     public String helloGetRequestParam(@RequestParam(required = false) String name, int age) {
@@ -38,5 +38,37 @@ public class RequestController {
     @ResponseBody
     public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/model
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestBodyForm(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
+    @GetMapping("/form/param/model")
+    @ResponseBody
+    public String helloRequestParam(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/json
+    // Header
+    //  Content type: application/json
+    // Body
+    //  {"name":"Robbie","age":"95"}
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) {
+        return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name, star.age);
     }
 }
---
+++ src/main/java/com/sparta/springprepare/request/Star.java
@@ -0,0 +1,16 @@
+package com.sparta.springprepare.request;
+
+import lombok.Setter;
+
+@Setter
+public class Star {
+
+    String name;
+    int age;
+
+//    public Star(int age, String name) {
+//        this.age = age;
+//        this.name = name;
+//    }
+
+}
