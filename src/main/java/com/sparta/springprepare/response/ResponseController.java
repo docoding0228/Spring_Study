1c2708c7e8da7e710ac9586e8ddfb1f9feaf5cbb
docoding0228 <npng0228@naver.com>
2024-05-16 14:41:14 +0900

    데이터를 Client에 반환하는 방법

---
+++ src/main/java/com/sparta/springprepare/response/ResponseController.java
@@ -0,0 +1,33 @@
+package com.sparta.springprepare.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/response")
+public class ResponseController {
+    // [Response header]
+    //   Content-Type: text/html
+    // [Response body]
+    //   {"name":"Robbie","age":95}
+    @GetMapping("/json/string")
+    @ResponseBody
+    public String helloStringJson() {
+        return "{\"name\":\"Robbie\",\"age\":95}";
+    }
+
+    // ctrl + e + enter
+    // [Response header]
+    //   Content-Type: application/json
+    // [Response body]
+    //   {"name":"Robbie","age":95}
+    @GetMapping("/json/class")
+    @ResponseBody
+    public Star helloClassJson() {
+        return new Star("Robbie", 95);
+    }
+
+    // string - text 로 보임. class - json 형태로 보임.
+}
